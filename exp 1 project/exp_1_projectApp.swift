a459df5ecc832e22c5f3d8fb3948fdea044ab469
anne kwaro <anne>
2023-09-15 10:09:23 +0200

    Initial Commit

---
+++ exp 1 project/exp_1_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  exp_1_projectApp.swift
+//  exp 1 project
+//
+//  Created by Anne Kwaro on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct exp_1_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
